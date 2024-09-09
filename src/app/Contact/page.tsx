ab896c9bb5de7a54ade2c958dfcff6329b6f6620
Dolapo <akingbojudolly@gmail.com>
2024-09-09 15:29:19 +0100

    Comment Landing Page

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,113 +1,75 @@
-"use client";
+// import React from 'react';
+// import { FaGithub, FaDiscord, FaReddit, FaTwitter } from "react-icons/fa";
+// import Statistics from '../components/Statistics';
+// import Aboutus from '../components/About';
+// import Button from '../components/Button';
+// import Chooseus from '../components/Choose';
+// import Footer from '../components/Footer';
+// import Updates from '../components/Updates';
+// import Link from "next/link";
 
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import Modal from "../components/Modal";
-import { useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import Button from "../components/Button";
-import { FaArrowRightLong } from "react-icons/fa6";
+// const Home = () => {
+//   return (
+//     <div className='w-screen'>
+//       <div className='relative h-screen w-screen font-light'>
+//         <video
+//           autoPlay
+//           loop
+//           muted
+//           playsInline
+//           className="absolute inset-0 w-full h-full object-cover"
+//         >
+//           <source src="/Background.mp4" type='video/mp4'/>
+//         </video>
+//         <div className='absolute inset-0 bg-gradient-to-b from-black/60 to-black/0 z-10'>
+//           <div className='absolute z-10 h-screen bg-cover bg-center font-Sarabun'>
+//             <nav className='text-white flex flex-col sm:flex-row justify-around p-5 sm:p-10'>
+//               <div className='text-center sm:text-left'>
+//                 <h3 className='font-medium text-lg sm:text-xl'>Ethicalmine</h3>
+//               </div>
+//               <div className='mt-4 sm:mt-0'>
+//                 <ul className='flex flex-col sm:flex-row text-sm items-center sm:items-start'>
+//                   <li className='px-2 py-1'>Home</li>
+//                   <li className='px-2 py-1'>Solution</li>
+//                   <li className='px-2 py-1'>Blog</li>
+//                   <li className='px-2 py-1'>Contact Us</li>
+//                 </ul>
+//               </div>
+//               <div className='mt-4 sm:mt-0'>
+//                 <ul className='flex justify-center sm:justify-end'>
+//                   <li className='px-2 text-xl'><FaGithub /></li>
+//                   <li className='px-2 text-xl'><FaDiscord /></li>
+//                   <li className='px-2 text-xl'><FaReddit /></li>
+//                   <li className='px-2 text-xl'><FaTwitter /></li>
+//                 </ul>
+//               </div>
+//             </nav>
+//             <section className='h-4/6'>
+//               <div className='flex flex-col justify-center items-center text-center h-full'>
+//                 <div className='flex flex-col items-center w-4/5 sm:w-3/5'>
+//                   <h1 className='bg-gradient-to-r text-4xl sm:text-6xl font-bold bg-clip-text text-transparent mb-5'>
+//                     Revolutionizing the Mining Industry <span className='text-white'>with Blockchain</span>
+//                   </h1>
+//                   <p className='text-white w-full sm:w-4/6 tracking-widest text-sm font-light leading-6'>
+//                     Our technology ensures that every step of the mining supply chain is transparent, ethical, and sustainable.
+//                   </p>
+//                 </div>
+//                 <div className='flex flex-col sm:flex-row text-center mt-5'>
+//                   <Button><Link href="/Contact">Get Started</Link></Button>
+//                   <button className="py-3 px-10 my-3 sm:my-5 mx-1 text-black bg-gradient-tob2 rounded-md">Learn more</button>
+//                 </div>
+//               </div>
+//             </section>
+//           </div>
+//         </div>
+//       </div>
+//       <Statistics />
+//       <Aboutus />
+//       <Chooseus />
+//       <Updates />
+//       <Footer />
+//     </div>
+//   );
+// };
 
-interface formProps {
-  email: string;
-}
-
-function Page() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<formProps>();
-
-  const [isModal, setIsModal] = useState(false);
-
-  const onSubmit: SubmitHandler<formProps> = (data) => {
-    console.log(data);
-    setIsModal(true);
-    reset();
-  };
-
-  const removeModal = () => {
-    setIsModal(false);
-  };
-
-  return (
-    <div className="h-screen bg-gradient-tob3 text-white font-Sarabun font-light">
-      <nav className="flex flex-col sm:flex-row justify-between p-5 sm:p-10 mx-5 sm:mx-20">
-        <div>
-          <h3 className="font-medium text-xl">
-            <Link href="/">Ethicalmine</Link>
-          </h3>
-        </div>
-        <div>
-          <ul className="flex justify-center sm:justify-start flex-wrap">
-            <li className="px-2 text-blue-500">
-              <Image src="/simple-icons_x.svg" alt="X" width={15} height={10} />
-            </li>
-            <li className="px-2">
-              <Image
-                src="/devicon_linkedin.svg"
-                alt="Linkedin"
-                width={15}
-                height={10}
-              />
-            </li>
-            <li className="px-2">
-              <Image src="/bi_medium.svg" alt="Medium" width={15} height={10} />
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <section className="h-4/5">
-        <div className="flex flex-col justify-center items-center text-center h-full">
-          <div className="w-full sm:w-3/5 flex flex-col justify-center items-center text-white">
-            <p className="text-lg sm:text-base">Coming soon</p>
-            <h1 className="bg-gradient-to-r text-4xl sm:text-6xl font-bold bg-clip-text text-transparent my-3">
-              Revolutionizing the Mining Industry{" "}
-              <span className="text-white">with Blockchain</span>{" "}
-            </h1>
-            <div className="w-full sm:w-4/5 text-center">
-              <p className="tracking-widest text-sm font-light leading-6">
-                Our technology ensures that every step of the mining supply chain
-                is transparent, ethical, and sustainable.
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col sm:flex-row items-center justify-center w-full sm:w-3/5 m-5">
-            <Button>Watch Video</Button>
-            <form className="w-full sm:w-2/4 relative text-center" onSubmit={handleSubmit(onSubmit)}>
-              <input
-                className="p-3 w-full rounded-md focus:outline-none"
-                type="email"
-                placeholder="Please enter your email address"
-                {...register("email", { required: "Enter valid Email address" })}
-              />
-              <button
-                type="submit"
-                className="absolute right-2 top-2 sm:right-5 bg-gradient-tob p-2 rounded-full cursor-pointer"
-              >
-                <FaArrowRightLong />
-              </button>
-              {errors.email && (
-                <div className="text-red-500 text-xs">{errors.email.message}</div>
-              )}
-            </form>
-          </div>
-          <div>
-            <p className="text-sm">Notify me when the app is launched</p>
-          </div>
-        </div>
-      </section>
-      <footer className="mx-5 sm:mx-20 mb-10 text-center sm:text-left">
-        <p className="text-xs sm:text-sm font-thin">
-          <span className="text-lg font-bold">800+</span> Partners across African countries
-        </p>
-      </footer>
-      {isModal && <Modal onClick={removeModal} />}
-    </div>
-  );
-}
-
-export default Page;
+// export default Home;
